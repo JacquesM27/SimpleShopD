106f65d843f250769a3fc8df808b360b2bf67302
Jakub <42150609+JacquesM27@users.noreply.github.com>
2023-10-03 19:14:25 +0200

    added user tests

--- Tests/SimpleShopD.Domain.Tests/UserTests.cs
+++ Tests/SimpleShopD.Domain.Tests/UserTests.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using SimpleShopD.Domain.Enum;
 using SimpleShopD.Domain.Shared.Exceptions;
+using SimpleShopD.Domain.Shared.ValueObjects;
 using SimpleShopD.Domain.Users;
 using SimpleShopD.Domain.Users.Exceptions;
 using System.Net.Mail;
@@ -19,7 +20,7 @@ namespace SimpleShopD.Domain.Tests
             string password = "12345Abc@";
 
             // Act
-            var exception = Record.Exception(() => new User(Guid.NewGuid(), new Shared.ValueObjects.Fullname(firstName,
+            var exception = Record.Exception(() => new User(Guid.NewGuid(), new Fullname(firstName,
                 lastName), emailAddress, password, UserRole.User, null));
 
             // Assert
@@ -37,7 +38,7 @@ namespace SimpleShopD.Domain.Tests
             string password = "12345Abc@";
 
             // Act
-            var exception = Record.Exception(() => new User(Guid.NewGuid(), new Shared.ValueObjects.Fullname(firstName,
+            var exception = Record.Exception(() => new User(Guid.NewGuid(), new Fullname(firstName,
                 lastName), emailAddress, password, UserRole.User, null));
 
             // Assert
@@ -57,7 +58,7 @@ namespace SimpleShopD.Domain.Tests
             string password = "12345Abc@";
 
             // Act
-            var exception = Record.Exception(() => new User(Guid.NewGuid(), new Shared.ValueObjects.Fullname(firstName,
+            var exception = Record.Exception(() => new User(Guid.NewGuid(), new Fullname(firstName,
                 lastName), emailAddress, password, UserRole.User, null));
 
             // Assert
@@ -77,7 +78,7 @@ namespace SimpleShopD.Domain.Tests
             string password = "12345Abc@";
 
             // Act
-            var exception = Record.Exception(() => new User(Guid.NewGuid(), new Shared.ValueObjects.Fullname(firstName,
+            var exception = Record.Exception(() => new User(Guid.NewGuid(), new Fullname(firstName,
                 lastName), emailAddress, password, UserRole.User, null));
 
             // Assert
@@ -95,7 +96,7 @@ namespace SimpleShopD.Domain.Tests
             string password = "12345Abc@";
 
             // Act
-            var exception = Record.Exception(() => new User(Guid.NewGuid(), new Shared.ValueObjects.Fullname(firstName,
+            var exception = Record.Exception(() => new User(Guid.NewGuid(), new Fullname(firstName,
                 lastName), emailAddress, password, UserRole.User, null));
 
             // Assert
@@ -113,12 +114,106 @@ namespace SimpleShopD.Domain.Tests
             string password = string.Empty;
 
             // Act
-            var exception = Record.Exception(() => new User(Guid.NewGuid(), new Shared.ValueObjects.Fullname(firstName,
+            var exception = Record.Exception(() => new User(Guid.NewGuid(), new Fullname(firstName,
                 lastName), emailAddress, password, UserRole.User, null));
 
             // Assert
             exception.Should().BeOfType<PasswordPolicyException>();
             exception.Message.Should().Be($"The password does not meet security requirements. It should have a minimum of 8 characters, contain lowercase and uppercase letters, numbers and special characters");
+        }
+
+        [Fact]
+        public void NewUser_ForIncorrectPassword_ShouldThrowPasswordPolicyException()
+        {
+            // Arrange
+            string firstName = "John";
+            string lastName = "Doe";
+            string emailAddress = "john.doe@ssd.com";
+            string password = "JohnDoe1222334234234";
+
+            // Act
+            var exception = Record.Exception(() => new User(Guid.NewGuid(), new Fullname(firstName,
+                lastName), emailAddress, password, UserRole.User, null));
+
+            // Assert
+            exception.Should().BeOfType<PasswordPolicyException>();
+            exception.Message.Should().Be($"The password does not meet security requirements. It should have a minimum of 8 characters, contain lowercase and uppercase letters, numbers and special characters");
+        }
+
+        [Theory]
+        [InlineData("", "Warsaw", "00-000", "Złota", "1", "Country cannot be empty", typeof(EmptyAddressParameterException))]
+        [InlineData("Poland", "", "00-000", "Złota", "1", "City cannot be empty", typeof(EmptyAddressParameterException))]
+        [InlineData("Poland", "Warsaw", "", "Złota", "1", "Zipcode cannot be empty", typeof(EmptyAddressParameterException))]
+        [InlineData("Poland", "Warsaw", "00-000", "", "1", "Street cannot be empty", typeof(EmptyAddressParameterException))]
+        [InlineData("Poland", "Warsaw", "00-000", "Złota", "", "Building number cannot be empty", typeof(EmptyAddressParameterException))]
+        public void NewUser_ForInvalidAddress_ShouldThrowEmptyAddressException(string country, string city, string zipCode, string street, string buildingNumber, string message, Type exceptionType)
+        {
+            // Arrange
+            string firstName = "John";
+            string lastName = "Doe";
+            string emailAddress = "john.doe@ssd.com";
+            string password = "JohnDoe123!";
+
+            // Act
+            var exception = Record.Exception(() => new User(Guid.NewGuid(), new Fullname(firstName,
+                lastName), emailAddress, password, UserRole.User, new HashSet<Address>() { new Address(country, city, zipCode, street, buildingNumber) }));
+
+            // Assert
+            exception.Should().BeOfType(exceptionType);
+            exception.Message.Should().Be(message);
+        }
+
+        [Fact]
+        public void NewUser_ForDuplicatedAddresses_ShouldContainsUniqueAddresses()
+        {
+            // Arrange
+            string firstName = "John";
+            string lastName = "Doe";
+            string emailAddress = "john.doe@ssd.com";
+            string password = "JohnDoe123!";
+            List<Address> addresses = new()
+            {
+                new Address("Poland", "Warsaw", "00-000", "Złota", "1"),
+                new Address("Poland", "Warsaw", "00-000", "Złota", "1")
+            };
+
+            // Act
+            var user = new User(Guid.NewGuid(), new Fullname(firstName,
+                lastName), emailAddress, password, UserRole.User, new HashSet<Address>(addresses));
+
+            // Assert
+            user.Addresses.Should().Equal(addresses.Distinct());
+        }
+
+        [Fact]
+        public void NewUser_ForValidData_ShouldCreateANewUser()
+        {
+            // Arrange
+            string firstName = "John";
+            string lastName = "Doe";
+            string emailAddress = "john.doe@ssd.com";
+            string password = "JohnDoe123!";
+            List<Address> addresses = new()
+            {
+                new Address("Poland", "Warsaw", "00-000", "Złota", "1"),
+                new Address("Poland", "Warsaw", "00-000", "Złota", "2")
+            };
+
+            // Act
+            var user = new User(Guid.NewGuid(), new Fullname(firstName,
+                lastName), emailAddress, password, UserRole.User, new HashSet<Address>(addresses));
+
+            // Assert
+            user.Addresses.Should().Equal(addresses);
+            user.Fullname.Firstname.Should().Be(firstName);
+            user.Fullname.Lastname.Should().Be(lastName);
+            user.Email.EmailAddress.Should().Be(emailAddress);
+            user.Password.VerifyPassword(password).Should().BeTrue();
+            user.Status.Value.Should().Be(AccountStatus.Inactive);
+            user.RoleOfUser.UserRole.Should().Be(UserRole.User);
+            user.ActivationToken!.Value.Should().NotBeNull();
+            user.ResetPasswordToken.Should().BeNull();
+            user.RefreshToken.Should().BeNull();
         }
 
         private static string GenerateRandomText(int length)
